f561d85ed60010314da5a5d12291d471510d087b
chrisos10 <j.dufitumuk@alustudent.com>
2023-11-10 10:14:11 -0800

    Print top ten posts

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1-main
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    1-main
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'cynt user agent 1.1'})
+
+    if res.status_code != 200:
+        print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
