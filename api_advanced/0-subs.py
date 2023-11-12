4c68a5153154ad24be6412b1341d5040907e270d
chrisos10 <j.dufitumuk@alustudent.com>
2023-11-11 21:57:09 -0800

    Returning the number of subscribers

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     and returns the total number of subscribers.
     Returns 0 if the subreddit is invalid
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyApp 0.1 (by j.dufitumuk@alustudent.com)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
